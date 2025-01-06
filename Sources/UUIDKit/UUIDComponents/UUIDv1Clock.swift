b51a50e27e127ef13f1ebe5cff90caff0b47ae0b
Basile Cornet <basile@cornet.im>
2025-01-06 09:52:26 +0100

    Add UUIDv6 support

---
+++ Sources/UUIDKit/UUIDComponents/UUIDv1Clock.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+internal final class Clock {
+    func next() -> (timestamp: Timestamp, clockSequence: ClockSequence) {
+        queue.sync {
+            let timestamp = Timestamp()
+            if timestamp <= lastTimestamp {
+                clockSequence = clockSequence.next
+            }
+            lastTimestamp = timestamp
+            return (timestamp, clockSequence)
+        }
+    }
+    
+    static let `default`: Clock = .init()
+    
+    private let queue: DispatchQueue = .init(label: "UUIDKit.Clock")
+    private var clockSequence: ClockSequence = .random()
+    private var lastTimestamp: Timestamp = .zero
+}
